5e0cca071f022c88a64944af0676950404da8396
Dave Green <hello@davetgreen.me>
2018-02-08 21:10:28 +0000

    Update plugin.php

--- kapow-core/plugin.php
+++ kapow-core/plugin.php
@@ -52,7 +52,7 @@ if ( ! empty( $_SERVER['HTTP_HOST'] ) && $_SERVER['REQUEST_URI'] ) {
 	$tld = explode( '.', wp_parse_url( $url, PHP_URL_HOST ) );
 	$tld = end( $tld );
 } else {
-	$tld = 'dev';
+	$tld = 'local';
 }
 // @codingStandardsIgnoreEnd
 
